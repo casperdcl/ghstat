12b67ce0c95ff94a38232c1476336f42fcc1ac2e
Casper da Costa-Luis <casper.dcl@physics.org>
2020-12-16 02:52:36 +0000

    add basic language stats

---
+++ ghstat.py
@@ -0,0 +1,76 @@
+import sys, os, collections, re, functools
+import matplotlib
+
+matplotlib.use("agg")
+import matplotlib.pyplot as plt, tqdm, yaml
+
+
+def ccycle():
+    while True:
+        for i in range(10):
+            yield "C%d" % i
+
+
+lang_names = {}
+lang_colours = {}
+for lang, v in yaml.safe_load(open("languages.yml")).items():
+    for ext in v.get("extensions", v.get("filenames", [])):
+        lang_names[ext.lstrip(".")] = lang
+        lang_colours[lang] = v.get("color", None)
+
+clean = functools.partial(re.compile(r"\{.*? => (.*?)\}").sub, r"\1")
+clean_whole = functools.partial(re.compile(r".*? => (.*?)").sub, r"\1")
+stats = sum(
+    (
+        collections.Counter((lang_names.get(lang, lang),) * int(c))
+        for i in sys.stdin
+        for c, f in [i.split("\t")[::2]]
+        if c != "-"
+        for lang in [
+            os.path.basename(clean_whole(clean(f.rstrip()))).rsplit(".", 1)[-1]
+        ]
+    ),
+    collections.Counter(),
+)
+
+d = sorted(
+    ((k, v) for k, v in stats.items() if k.lower() not in {"licence", "license"}),
+    key=lambda kv: kv[1],
+    reverse=True,
+)
+print(d)
+
+# plt.barh([i for i, _ in d], [i for _, i in d], log=True)
+# plt.xlabel("inserted lines")
+# plt.ylabel("extension")
+
+thresh = sum(v for _, v in d) * 0.005
+o = [(k, v) for k, v in d if v < thresh]
+d = [(k, v) for k, v in d if v >= thresh] + [("Other", sum(dict(o).values()))]
+
+plt.figure(figsize=(14, 7))
+
+plt.subplot(121)
+c = ccycle()
+plt.pie(
+    [v for _, v in d],
+    labels=[
+        "{} {}".format(k, tqdm.tqdm.format_sizeof(v) if v > 99 else v) for k, v in d
+    ],
+    colors=[lang_colours.get(k) or next(c) for k, _ in d],
+)
+plt.title("Inserted lines (99.5%)")
+
+plt.subplot(122)
+c = ccycle()
+plt.pie(
+    [v for _, v in o],
+    labels=[
+        "{} {}".format(k, tqdm.tqdm.format_sizeof(v) if v > 99 else v) for k, v in o
+    ],
+    colors=[lang_colours.get(k) or next(c) for k, _ in o],
+)
+plt.title("Other (0.5%)")
+
+plt.tight_layout()
+plt.savefig("ghstats.png")
---
+++ ghstat.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -eo pipefail
+usage() {
+  cat <<EOF
+Usage:
+  ghstat.sh
+Env:
+  REPO_SLUGS
+  AUTHOR
+  GITHUB_TOKEN
+EOF
+}
+if [[ -z "$REPO_SLUGS" || -z "$AUTHOR" || -z "$GITHUB_TOKEN" ]]; then
+  usage
+  exit 1
+fi
+this="$(dirname $0)"
+
+echo '[1/2] clone ...'
+for repo in ${REPO_SLUGS}; do
+  [[ -d "$this/$repo" ]] || git clone --single-branch https://${GITHUB_TOKEN}@github.com/$repo "$this/$repo"
+done >>/dev/null
+
+echo '[2/2] processing ...'
+[[ -f languages.yml ]] || wget https://github.com/github/linguist/raw/master/lib/linguist/languages.yml
+for repo in ${REPO_SLUGS}; do
+  git -C "$this/$repo" log --format="" -M -C -C --author="$AUTHOR" --numstat
+done |
+  python -m tqdm --unit commits |
+  python "$this/ghstat.py"
