367294fb63da7b477e21a30e01db88c816cb5fdc
Casper da Costa-Luis <casper.dcl@physics.org>
2020-12-16 21:22:43 +0000

    ghstat: back to basics

--- ghstat.py
+++ ghstat.py
@@ -1,4 +1,4 @@
-import sys, os, collections, re, functools, subprocess
+import sys, os, collections, re, functools
 import matplotlib
 
 matplotlib.use("agg")
@@ -16,40 +16,42 @@ lang_colours = {}
 langs = yaml.safe_load(open("languages.yml"))
 for lang, v in langs.items():
     for ext in v.get("extensions", v.get("filenames", [])):
-        lang_names[ext.lstrip(".")] = lang
+        ext = ext.lstrip(".")
+        if ext in lang_names:
+            lang_names[ext] = ext
+            lang_colours[ext] = None
+        else:
+            lang_names[ext] = lang
         lang_colours[lang] = v.get("color", None)
-
-
-re_lang = re.compile(r"^\s*language:\s+(.*)", flags=re.M).search
-
-
-def fn2lang(fn):
-    try:
-        assert os.path.isfile(fn)
-        res = subprocess.check_output(["github-linguist", fn]).decode("U8")
-        return re_lang(res).group(1)
-    except:
-        ext = os.path.basename(fn).rsplit(".", 1)[-1]
-        return lang_names.get(ext, ext)  # TODO actually use fn & langs.*.
-
+lang_names.update(md="Markdown", mmd="Markdown", txt="Text", cfg="INI")
+lang_names["1"] = "Roff"
 
 clean = functools.partial(re.compile(r"\{.*? => (.*?)\}").sub, r"\1")
 clean_whole = functools.partial(re.compile(r".*? => (.*?)").sub, r"\1")
-stats = sum(
-    (
-        collections.Counter((fn2lang(clean_whole(clean(f.rstrip()))),) * int(c))
-        for i in sys.stdin
-        for c, f in [i.split("\t")[::2]]
-        if c != "-"
-    ),
-    collections.Counter(),
-)
+
+
+def fn2lang(fn):
+    fn = os.path.basename(clean_whole(clean(fn.rstrip())))
+    if "." in fn:
+        base, ext = fn.rsplit(".", 1)
+    else:
+        base, ext = "", fn
+    if base.lower().startswith("cmake") and ext.lower() == "txt":
+        return "CMake"
+    return lang_names.get(ext, ext)
+
+
+stats = collections.Counter()
+for k, v in (
+    (fn2lang(f), int(c)) for i in sys.stdin for c, f in [i.split("\t")[::2]] if c != "-"
+):
+    stats[k] += v
 
 d = sorted(
     (
         (k, v)
         for k, v in stats.items()
-        if k.lower() not in {"licence", "license", "postscript", "csv", "svg",}
+        if k.lower() not in {"licence", "license", "postscript", "csv", "svg"}
     ),
     key=lambda kv: kv[1],
     reverse=True,
